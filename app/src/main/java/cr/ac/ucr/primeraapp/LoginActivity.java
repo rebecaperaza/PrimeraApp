f6bb699f2a4a9e350061094ca54a12a71d344bc9
rebecaperaza <66180590+rebecaperaza@users.noreply.github.com>
2020-08-17 14:15:08 -0600

    Initial commit

---
+++ app/src/main/java/cr/ac/ucr/primeraapp/LoginActivity.java
@@ -0,0 +1,72 @@
+package cr.ac.ucr.primeraapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText etEmail;
+    private EditText etPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etEmail = findViewById(R.id.et_email);
+        etPassword = findViewById(R.id.et_password);
+
+    }
+
+    @Override
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.btn_login:
+                login();
+                break;
+            case R.id.btn_goto_register:
+                gotoregister();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void login(){
+        String email = etEmail.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
+
+        if(email.isEmpty()){
+            etEmail.setError(getString(R.string.error_email));
+            return;
+        }
+
+        if (password.isEmpty()){
+            etPassword.setError(getString(R.string.error_password));
+            return;
+        }
+
+        // TODO: se tiene que sustituir con la logica de autenticacion de la aplicacion
+        if (email.equalsIgnoreCase("admin@email.com") && "123".equalsIgnoreCase(password)){
+            // TODO: enviarlo al main activity
+            // TODO: almacenar en el storage el usuario logueado
+            Toast.makeText(this, R.string.logged_in, Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, MainActivity.class );
+            startActivity(intent);
+            finish();
+        }else{
+            Toast.makeText(this, R.string.no_match, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void gotoregister(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
---
+++ app/src/main/java/cr/ac/ucr/primeraapp/RegisterActivity.java
@@ -0,0 +1,19 @@
+package cr.ac.ucr.primeraapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        EditText etNam
+
+    }
+}
